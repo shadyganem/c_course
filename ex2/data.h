6997c008d885387b050d18d97f1ed3d0b8b33f4b
Shady Ganem <shadyganem@gmail.com>
2021-01-11 20:31:35 +0200

    mod - created a function for each primitive data type

--- ex2/data.h
+++ ex2/data.h
@@ -1,3 +1,15 @@
 
+char create_random_char(void);
+unsigned char create_random_unsigned_char(void);
+short create_random_short(void);
+unsigned short create_random_unsigned_short(void);
 int create_random_int(void);
+unsigned int create_random_unsigned_int(void);
+long create_random_long(void);
+unsigned long create_random_unsigned_long(void);
+float create_random_float(void);
+double create_random_double(void);
+long double create_random_long_double(void);
+
+
 
--- ex2/main.c
+++ ex2/main.c
@@ -2,9 +2,32 @@
 #include <stdio.h>
 #include "data.h"
 
+static void PrintIntro(const char *str)
+{
+	for (int i=0; i < 80; i++)
+	{
+		printf("-");
+	}
+	printf("\n");
+	printf("%44s\n", str);
+	for (int i=0; i < 80; i++)
+	{
+		printf("-");
+	}
+	printf("\n");
+}
+
 int main(int argc, char *charv[])
 {
-	printf("data types in C\n");
-	int a = create_random_int();
-	printf("a = %d\n", a);
+	PrintIntro("Data Types in C");
+	char character = create_random_char();
+	printf("character = %c\n", character);
+	unsigned char U_character = create_random_unsigned_char();
+	printf("unsigned charater = %c\n", U_character);
+	int integer = create_random_int();
+	printf("integer = %d\n", integer);
+	float floating_piont = create_random_float();
+	printf("floating point = %f\n", floating_piont);
+
+	return 0;
 }
